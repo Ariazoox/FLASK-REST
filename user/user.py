ca1b875ddb2b9a300e12ff97c6df160274f4ed7a
Ariazoox <101866115+Ariazoox@users.noreply.github.com>
2023-11-20 17:17:22 +0100

    Update user.py

--- user/user.py
+++ user/user.py
@@ -12,53 +12,52 @@ with open('{}/databases/users.json'.format("."), "r") as jsf:
    users = json.load(jsf)["users"]
    print(users)
 
-
-# point d'entrée qui renvoie à la page d'accueil "Welcome to the User service!"
+# Point d'entrée qui renvoie à la page d'accueil "Welcome to the User service!"
 @app.route("/", methods=['GET'])
 def home():
    return "<h1 style='color:blue'>Welcome to the User service!</h1>"
 
-# point d'entrée qui affiche l'intégralité des utilisateurs de notre fichier users.json
+# Point d'entrée qui affiche l'intégralité des utilisateurs de notre fichier users.json
 @app.route("/users", methods=['GET'])
 def get_json():
     res = make_response(jsonify(users), 200)
-    # Renvoie la liste des users
+    # Renvoie la liste des utilisateurs
     return res
 
-# point d'entrée pour afficher un utilisateur selon son ID
+# Point d'entrée pour afficher un utilisateur selon son ID
 @app.route("/users/<userid>", methods=['GET'])
 def get_user_byid(userid):
-    # Boucle qui parcourt nos users
+    # Boucle qui parcourt nos utilisateurs
     for user in users:
         # Vérification de la correspondance des ID
         if str(user["id"]) == str(userid):
-            res = make_response(jsonify(user),200)
-            # Renvoie le user avec l'ID renseigné
+            res = make_response(jsonify(user), 200)
+            # Renvoie l'utilisateur avec l'ID renseigné
             return res
     # Message d'erreur au cas où l'ID serait inexistant
-    return make_response(jsonify({"error":"User ID not found"}),400)
+    return make_response(jsonify({"error": "User ID not found"}), 400)
 
-# Point d'accès qui affiche pour un utilisateur donné, ses résérvations.
+# Point d'accès qui affiche, pour un utilisateur donné, ses réservations.
 @app.route("/users/<userid>/bookings", methods=["GET"])
 def get_user_bookings(userid):
-    # requête pour appeler le service booking par le biais de son API REST
+    # Requête pour appeler le service booking par le biais de son API REST
     res = requests.get("http://localhost:3201/bookings/{}".format(userid))
     booking = res.json()
-    # Boucle qui parcourt les dates dans nos bookings
+    # Boucle qui parcourt les dates dans nos réservations
     for date_item in booking["dates"]:
         mapped_movies = []
-        # Boucle qui parcourt les movies
+        # Boucle qui parcourt les films
         for movie_id in date_item["movies"]:
-            # requête pour appeler le service movie par le biais de son API REST
+            # Requête pour appeler le service movie par le biais de son API REST
             res = requests.get("http://localhost:3200/movies/{}".format(movie_id))
             movie = res.json()
-            moviedirector= movie["director"]
+            movie_director = movie["director"]
 
-            mapped_movies.append({"director" : moviedirector})
+            mapped_movies.append({"director": movie_director})
         date_item["movies"] = mapped_movies
-    # Renvoie les résérvations de l'utilisateur dont l'ID a été renseigné
+    # Renvoie les réservations de l'utilisateur dont l'ID a été renseigné
     return make_response(jsonify(booking), res.status_code)
 
 if __name__ == "__main__":
-   print("Server running in port %s"%(PORT))
+   print("Server running on port %s"%(PORT))
    app.run(host=HOST, port=PORT)
